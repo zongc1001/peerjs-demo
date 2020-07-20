af0c9bc0ace7871f01724f6777d3b156c38da388
MATHISON <32095921+zongc1001@users.noreply.github.com>
2020-07-20 16:46:20 +0800

    update

--- src/receive.js
+++ src/receive.js
@@ -54,49 +54,6 @@ import peerjs from 'peerjs';
         {
           urls: iceServers.urls, username: iceServers.username,
           credential: iceServers.credential
-        },
-        { url: 'stun:stun.turnservers.com:3478' },
-        { url: 'stun:stun01.sipphone.com' },
-        { url: 'stun:stun.ekiga.net' },
-        { url: 'stun:stun.fwdnet.net' },
-        { url: 'stun:stun.ideasip.com' },
-        { url: 'stun:stun.iptel.org' },
-        { url: 'stun:stun.rixtelecom.se' },
-        { url: 'stun:stun.schlund.de' },
-        { url: 'stun:stun.l.google.com:19302' },
-        { url: 'stun:stun1.l.google.com:19302' },
-        { url: 'stun:stun2.l.google.com:19302' },
-        { url: 'stun:stun3.l.google.com:19302' },
-        { url: 'stun:stun4.l.google.com:19302' },
-        { url: 'stun:stunserver.org' },
-        { url: 'stun:stun.softjoys.com' },
-        { url: 'stun:stun.voiparound.com' },
-        { url: 'stun:stun.voipbuster.com' },
-        { url: 'stun:stun.voipstunt.com' },
-        { url: 'stun:stun.voxgratia.org' },
-        { url: 'stun:stun.xten.com' },
-        { url: 'turn:homeo@turn.bistri.com:80', credential: 'homeo' },
-        { url: 'stun:47.95.119.173:3478' },
-        { url: 'turn:0.peerjs.com:3478' },
-        {
-          url: 'turn:47.95.119.173:3478',
-          username: 'zongchen',
-          credential: 'onmyown0.',
-        },
-        {
-          url: 'turn:numb.viagenie.ca',
-          credential: 'muazkh',
-          username: 'webrtc@live.com'
-        },
-        {
-          url: 'turn:192.158.29.39:3478?transport=udp',
-          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-          username: '28224511:1379330808'
-        },
-        {
-          url: 'turn:192.158.29.39:3478?transport=tcp',
-          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-          username: '28224511:1379330808'
         }
       ],
     })
@@ -110,7 +67,7 @@ import peerjs from 'peerjs';
       } else {
         lastPeerId = peer.id;
       }
-
+      console.log(peer);
       console.log('ID: ' + peer.id);
       recvId.innerHTML = 'ID: ' + peer.id;
       status.innerHTML = 'Awaiting connection...';
@@ -271,9 +228,7 @@ import peerjs from 'peerjs';
       console.log('Sent: ' + msg)
       addMessage('<span class="selfMsg">Self: </span>' + msg)
     } else {
-      conn = peer.connect(peerId, {
-        reliable: true,
-      })
+      
       console.log('Connection is closed')
     }
   })
